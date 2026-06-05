0389e71cf376d1e50f9813d71909b3799a4e37c6
Siva2583 <kgsivacharan2005@gmail.com>
2026-06-05 19:43:39 +0530

    Update AlgoViz header and footer

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -7,24 +7,18 @@ const ModeToggle = () => {
   return (
     <div className={`dll-toggle ${activeMode === 'algo' ? 'algo-mode' : 'pattern-mode'}`}>
       
-      {/* Node 1: Algo (Cyan) */}
       <div 
         className={`dll-node ${activeMode === 'algo' ? 'active-algo' : ''}`}
         onClick={() => setActiveMode('algo')}
       >
         <span>Algo Visualizer</span>
       </div>
-
-      {/* The Connector: Two Stacked Arrows */}
       <div className="dll-arrows">
-         {/* Top Arrow: Points Left (For Algo) */}
          <div className="arrow-line arrow-top">&larr;</div>
          
-         {/* Bottom Arrow: Points Right (For Pattern) */}
          <div className="arrow-line arrow-bottom">&rarr;</div>
       </div>
 
-      {/* Node 2: Pattern (Amber) */}
       <div 
         className={`dll-node ${activeMode === 'pattern' ? 'active-pattern' : ''}`}
         onClick={() => setActiveMode('pattern')}
